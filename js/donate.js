f566abab85d909b4fbc2459f0d5180903b2d7cf7
Zia <ziair3812@gmail.com>
2024-09-24 21:47:57 +0600

    first commit

---
+++ js/donate.js
@@ -0,0 +1,84 @@
+//Noakhali 
+
+
+document.getElementById('donateNoakhali').addEventListener('click',function(){
+    let DonateAmount = Number(document.getElementById('noakhaliDonation').value)
+    let TotalFunding = Number(document.getElementById('totalNoakhaliFund').innerText)
+    let balance =Number(document.getElementById('mainBalance').innerText)
+    if(DonateAmount >= 0){
+        if(DonateAmount <= balance){
+            if(!isNaN(DonateAmount)){
+                balance = balance - DonateAmount;
+                TotalFunding = TotalFunding + DonateAmount;
+                console.log(balance);
+                console.log(TotalFunding);
+                document.getElementById('mainBalance').innerText = balance;
+                document.getElementById('totalNoakhaliFund').innerText = TotalFunding
+            }else{
+                alert('Not possible to donate')
+            }
+           }else{
+                alert('Not possible to donate')
+           }
+    }else{
+        alert('Not possible to donate')
+    }
+   
+    
+})
+
+//Feni
+
+document.getElementById('donateFeni').addEventListener('click',function(){
+    let DonateAmount = Number(document.getElementById('feniDonation').value)
+    let TotalFunding = Number(document.getElementById('totalfeniFund').innerText)
+    let balance =Number(document.getElementById('mainBalance').innerText)
+    if(DonateAmount >= 0){
+        if(DonateAmount <= balance){
+            if(!isNaN(DonateAmount)){
+                balance = balance - DonateAmount;
+                TotalFunding = TotalFunding + DonateAmount;
+                console.log(balance);
+                console.log(TotalFunding);
+                document.getElementById('mainBalance').innerText = balance;
+                document.getElementById('totalfeniFund').innerText = TotalFunding
+            }else{
+                alert('Not possible to donate')
+            }
+           }else{
+                alert('Not possible to donate')
+           }
+    }else{
+        alert('Not possible to donate')
+    }
+   
+    
+})
+
+document.getElementById('Donate').addEventListener('click',function(){
+    let DonateAmount = Number(document.getElementById('noakhaliDonation').value)
+    let TotalFunding = Number(document.getElementById('totalNoakhaliFund').innerText)
+    let balance =Number(document.getElementById('mainBalance').innerText)
+    if(DonateAmount >= 0){
+        if(DonateAmount <= balance){
+            if(!isNaN(DonateAmount)){
+                balance = balance - DonateAmount;
+                TotalFunding = TotalFunding + DonateAmount;
+                console.log(balance);
+                console.log(TotalFunding);
+                document.getElementById('mainBalance').innerText = balance;
+                document.getElementById('totalNoakhaliFund').innerText = TotalFunding
+            }else{
+                alert('Not possible to donate')
+            }
+           }else{
+                alert('Not possible to donate')
+           }
+    }else{
+        alert('Not possible to donate')
+    }
+   
+    
+})
+
+
